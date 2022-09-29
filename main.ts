d85de12cfac85bf25f77ce087b07cc45295300b7
ethan-stone <ethan.stone9352@gmail.com>
2022-09-28 23:55:35 -0400

    experimenting with mongodb timeouts

---
+++ main.ts
@@ -0,0 +1,52 @@
+import * as dotenv from "dotenv";
+import { MongoClient } from "mongodb";
+import { randomUUID } from "crypto";
+
+dotenv.config();
+
+const mongoUrl = process.env.DATABASE_URL;
+
+if (!mongoUrl) throw new Error("Missing mongo url");
+
+const mongoClient = new MongoClient(mongoUrl);
+
+type Log = {
+  metadata: { chargingStationId: string };
+  timestamp: Date;
+  log: Record<string, any>;
+};
+
+async function setTimeoutPromise(timeout: number) {
+  return new Promise((resolve) => {
+    setTimeout(resolve, timeout);
+  });
+}
+
+async function main() {
+  const db = mongoClient.db("logs");
+  const collection = await db.createCollection<Log>("logs", {
+    timeseries: {
+      timeField: "timestamp",
+      metaField: "metadata"
+    }
+  });
+
+  const logs: Log[] = [];
+  for (let i = 0; i < 1000; i++) {
+    logs.push({
+      metadata: { chargingStationId: randomUUID() },
+      timestamp: new Date(),
+      log: {
+        occpMessageType: "RemoteStartTransaction",
+        ocppMessageId: randomUUID(),
+        ocppMessagePayload: {}
+      }
+    });
+
+    await setTimeoutPromise(200);
+  }
+
+  await collection.insertMany(logs);
+}
+
+main();
